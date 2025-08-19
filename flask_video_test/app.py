e0af3812b06a7087ea6f16c490b0addaac441092
N-jiranut <jiranut.nat@gmail.com>
2025-08-19 22:00:21 +0700

    .

--- flask_video_test/app.py
+++ flask_video_test/app.py
@@ -1,27 +1,70 @@
-from flask import Flask, Response,  render_template
 import cv2
+from flask import Flask, render_template, Response
+import mediapipe as mp
+hands = mp.solutions.hands.Hands()
+pose = mp.solutions.pose.Pose()
+pose_take = [0,11,12,13,14,15,16]
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)  # 0 = default webcam
+cap = cv2.VideoCapture(0)  # 0 = default webcam
+
+def getminmax(array,side):
+    arx, ary = [], []
+    
+    for twin in array:
+        arx.append(twin[0])
+        ary.append(twin[1])
+    
+    mx = round(max(arx)*720)
+    lx = round(min(arx)*720)
+    my = round(max(ary)*480)
+    ly = round(min(ary)*480)
+    
+    cv2.rectangle(frame, (lx,ly), (mx,my),(50,150,0), 2)
+    cv2.putText(frame, str(side), (lx, ly), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2, cv2.LINE_AA)
 
 def generate_frames():
     while True:
-        success, frame = camera.read()
-        if not success:
+        ret, frame = cap.read()
+        if not ret:
+            print("cannot find camera")
             break
         else:
+            frame = cv2.resize(frame, (720, 480))
+            frame = cv2.flip(frame, 1)
+            frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+            hand_result = hands.process(frame)
+            if hand_result.multi_hand_landmarks:
+                for idx, hand_landmarks in enumerate(hand_result.multi_hand_landmarks):
+                    arx, ary = [], []
+                    handedness = hand_result.multi_handedness[idx].classification[0].label
+                    for lm in hand_landmarks.landmark: 
+                        x, y= lm.x, lm.y    
+                        arx.append(x)
+                        ary.append(y)
+
+                    mx = round(max(arx)*720)
+                    lx = round(min(arx)*720)
+                    my = round(max(ary)*480)
+                    ly = round(min(ary)*480)
+
+                    cv2.rectangle(frame, (lx,ly), (mx,my),(50,150,0), 2)
+                    cv2.putText(frame, str(handedness), (lx, ly), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2, cv2.LINE_AA)
+
             # Encode frame as JPEG
-            _, buffer = cv2.imencode('.jpg', frame)
+            ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
 
-            # Yield frame in byte format for MJPEG stream
+            # Use yield with multipart/x-mixed-replace for live stream
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/video')
 def video():
-    
-    # return render_template("video.html")
     return Response(generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
--- rahh.py
+++ rahh.py
@@ -1,82 +1,44 @@
-import cv2,time, os
-import mediapipe as mp 
+import cv2
+import mediapipe as mp
 hands = mp.solutions.hands.Hands()
+pose = mp.solutions.pose.Pose()
+pose_take = [0,11,12,13,14,15,16]
 
-path = "client_picture"
-listt = os.listdir(path)
+cap = cv2.VideoCapture(0)
 
-def forpredict():
-    LH = [0 for _ in range(42)]
-    RH = [0 for _ in range(42)]
-    BO = [0 for _ in range(14)]
-    row=[]
-    condition=0
-    con = 0
-    allcimg = len(picture)    
-    percimg = math.floor(allcimg/15)
-    for img in picture:
-        current_img_path = os.path.join(path, img)
-        cimg = cv2.imread(current_img_path)
-        if condition%percimg == 0 and con < 15:
-            cimg = cv2.resize(cimg, (720, 480))
-            hand_result = hands.process(cimg)
-            pose_results = pose.process(cimg)
-            if hand_result.multi_hand_landmarks:
-                RH=[]
-                LH=[]
-                for idx, hand_landmarks in enumerate(hand_result.multi_hand_landmarks):
-                    mp.solutions.drawing_utils.draw_landmarks(cimg,hand_landmarks,mp.solutions.hands.HAND_CONNECTIONS)
-                    handedness = hand_result.multi_handedness[idx].classification[0].label
-                    for lm in hand_landmarks.landmark: 
-                        x, y= lm.x, lm.y         
-                        if handedness == "Left" and len(LH)<42:
-                            LH.extend([x,y])
-                        if handedness == "Right" and len(RH)<42:
-                            RH.extend([x,y])
-                if len(LH) <= 0:
-                    LH = [0 for _ in range(42)]
-                row.extend(LH)
-                if len(RH) <= 0:
-                    RH = [0 for _ in range(42)]
-                row.extend(RH)
-
-                if pose_results.pose_landmarks:
-                    BO=[]
-                    landmarks = pose_results.pose_landmarks.landmark
-                    for id,lm in enumerate(landmarks):
-                        x, y = lm.x, lm.y
-                        if id in pose_take:
-                            cv2.circle(cimg ,(round(x*720),round(y*480)), 1, (0,0,255), 7)
-                            BO.extend([x, y])    
-                else:
-                    BO.extend([0 for _ in range(14)])
-                row.extend(BO)
-                con += 1
-
-            cv2.imshow("Video", cimg)
-        if cv2.waitKey(1) == ord('q'):
-           break
-        condition+=1
-
-    if con < 15:
-        foradd=[]
-        for object in [LH,RH,BO]:
-            foradd.extend(object)
-        row.extend(foradd*(15-con))
-
-    landmarks_np = np.array(row).reshape(1, -1)
-    pred = model.predict(landmarks_np)
-    index = np.argmax(pred)
-    label = class_names[index]
-
-    print(pred[0][index])
-    print(label)
-
-for pic in listt:
-    cpic = os.path.join(path, pic)
-    img = cv2.imread(cpic)
+def getminmax(array,side):
+    arx, ary = [], []
     
-    cv2.imshow("test",img)
+    for twin in array:
+        arx.append(twin[0])
+        ary.append(twin[1])
     
-    cv2.waitKey(1)
-    time.sleep(.05)
+    mx = round(max(arx)*720)
+    lx = round(min(arx)*720)
+    my = round(max(ary)*480)
+    ly = round(min(ary)*480)
+    
+    cv2.rectangle(frame, (lx,ly), (mx,my),(50,150,0), 2)
+    cv2.putText(frame, str(side), (lx, ly), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2, cv2.LINE_AA)
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (720, 480))
+    frame = cv2.flip(frame, 1)
+    frame = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+    hand_result = hands.process(frame)
+    if hand_result.multi_hand_landmarks:
+        for idx, hand_landmarks in enumerate(hand_result.multi_hand_landmarks):
+            lmlist=[]
+            handedness = hand_result.multi_handedness[idx].classification[0].label
+            for lm in hand_landmarks.landmark: 
+                x, y= lm.x, lm.y    
+                lmlist.append([x,y])
+            getminmax(lmlist, handedness)
+                     
+    
+    cv2.imshow("Video", frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+cv2.destroyAllWindows()
+cap.release()
